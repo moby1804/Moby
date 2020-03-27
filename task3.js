eb341ff8818b5b0dcf15f5bedee991fd944c5afe
Moby Okitikpi <mogbitseo@yahoo.com>
2020-03-27 18:08:14 +0100

    JS Task

---
+++ task3.js
@@ -0,0 +1,32 @@
+let name = 'Mogbitse'
+let course = ['html', 'css', 'javascript']
+
+console.log(name)
+console.log(course)
+
+let n = 200;
+
+if (name.length % 2 !==0){
+for ( i = 1;i<=n ; i++) {
+
+    if (i % 2 !==0){
+
+        console.log(i+ '')
+    
+}
+
+}
+}else{ 
+
+if (name.length % 2 ===0){
+    for ( i = 1;i<=n ; i++) {
+    
+        if (i % 2 ===0){
+    
+            console.log(i+ '')
+        
+    }
+    
+    }
+    } 
+}
